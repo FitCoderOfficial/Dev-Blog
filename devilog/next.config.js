7e628e8f3e45b5f146a16c069bf8933c06be4733
Shin-Sae-byeok <toqur1219@naver.com>
2022-12-31 11:53:17 +0900

    feat:setting tailwind css

--- devilog/next.config.js
+++ devilog/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  //nextjs 13 실험적인 기능 - appDir 새로운 폴더 사용 가용여부
+  experimental: {                 
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
--- devilog/tailwind.config.js
+++ devilog/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    
+    //실험적인 기능 추가했기 때문에 설정해줘야함
+    "./app/**/*.{js,ts,jsx,tsx}", 
+
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        notosans: ['NotoSans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
