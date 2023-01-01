07f1d7c59576a43c38811273b06861853888c4ab
Shin-Sae-byeok <toqur1219@naver.com>
2023-01-01 22:14:24 +0900

    feat:addming home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import logo from '../img/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={logo} art=""/>
+      <span>
+        Copyright 2023. devilog All right reserved.
+      </span>
+    </footer>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../img/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="links">
+          <Link className='link' to="/?cat=PYTHON">
+            <h6>PYTHON</h6>
+          </Link>
+          <Link className='link' to="/?cat=js">
+            <h6>JS</h6>
+          </Link> 
+          <Link className='link' to="/?cat=WEB3">
+            <h6>WEB3</h6>
+          </Link>
+          <Link className='link' to="/?cat=nextjs">
+            <h6>NEXT.JS</h6>
+          </Link>  
+          <Link className='link' to="/?cat=DJANGO">
+            <h6>DJANGO</h6>
+          </Link>  
+          <Link className='link' to="/?cat=FRONTEND">
+            <h6>FRONTEND</h6>
+          </Link>  
+          <Link className='link' to="/?cat=BACKEND">
+            <h6>BACKEND</h6>
+          </Link>
+          <span> SB </span>
+          <span> 로그아웃 </span>
+          <span className='write'>
+            <Link className='link' to='/write'>글쓰기</Link>
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
